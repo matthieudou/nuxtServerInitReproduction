55151c8c9016365142e5600dc61ee9dec35a94bc
matthieudou <matthieu.doultremont@gmail.com>
2019-08-28 17:57:25 +0200

    add links

--- pages/_other.vue
+++ pages/_other.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="container">
-    <h2 class="text-xl">Data should be seen</h2>
+  <div class="container p-12">
+    <h1 class="text-2xl font-bold mb-10">Other page</h1>
+    <h2 class="text-xl mb-8">Data should be seen</h2>
     <div class="p-8 rounded bg-gray-100 mx-auto shadow">
       {{ theData }}
     </div>
+
+    <nuxt-link class="mt-8 block text-blue-500" to="/">
+      Back home
+    </nuxt-link>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="container">
-    <h2 class="text-xl">Data should be seen</h2>
+  <div class="container p-12">
+    <h1 class="text-2xl font-bold mb-10">Home page</h1>
+    <h2 class="text-xl mb-8">Data should be seen</h2>
     <div class="p-8 rounded bg-gray-100 mx-auto shadow">
       {{ theData }}
     </div>
+
+    <nuxt-link class="mt-8 block text-blue-500" :to="{name: 'other', params: {other: 'test'}}">
+      Go to other page
+    </nuxt-link>
   </div>
 </template>
 
